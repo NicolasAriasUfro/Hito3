0623e30429ef8e2d1779cddefc031e3c4189263d
NicolasAriasUfro <101823845+NicolasAriasUfro@users.noreply.github.com>
2022-07-08 10:25:08 -0400

    Creacion de Archivos

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        compararFotografias();
+        mostrarResultados();
+    }
+
+    private static void mostrarResultados() {
+    }
+
+    private static void compararFotografias() {
+    }
+}
